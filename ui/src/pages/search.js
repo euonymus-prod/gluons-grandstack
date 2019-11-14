51755a13c1ff7877ca4d9669614d56206d15e3ad
euonymus <euonymus0220@gmail.com>
2019-11-14 19:59:28 +0900

    try

--- ui/src/pages/search.js
+++ ui/src/pages/search.js
@@ -1,25 +1,51 @@
 // react
-import React, { Component } from "react";
+import React from "react";
+// GraphQL
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
 // component
 import QuarkInList from "../components/quark-in-list";
+import SearchQuarkDemo from "../components/search-quark-demo";
 
-class Search extends Component {
-  componentDidMount() {
-    document.title = `Search Result of ${this.props.match.params.keywords} -\ngluons`;
+const SEARCH_QUARKS = gql`
+  query searchQuarks($first: Int, $keyword: String) {
+    searchQuarks(first: $first, keyword: $keyword) {
+      id
+      name
+      description
+      image_path
+    }
   }
+`;
 
-  render() {
-    const quark_property_caption = "test caption";
-    return (
-      <div className="container">
-        <h2>{quark_property_caption}</h2>
-        <div className="related">
-          <div className="well subject-relation white">
-            <QuarkInList />
+function Search(props) {
+  document.title = `Search Result of ${props.match.params.keywords} -\ngluons`;
+  const [rowsPerPage] = React.useState(10);
+
+  const { loading, data, error } = useQuery(SEARCH_QUARKS, {
+    variables: {
+      first: rowsPerPage,
+      keyword: "徳川"
+    }
+  });
+
+  const quark_property_caption = "test caption";
+  return (
+    <div className="container">
+      <h2>{quark_property_caption}</h2>
+      <div className="related">
+        {loading && !error && <p>Loading...</p>}
+        {error && !loading && <p>Error</p>}
+        {data && !loading && !error && (
+          <div>
+            {data.searchQuarks.map(n => {
+              return <QuarkInList data={n} />;
+            })}
           </div>
-        </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Search;
