fcd968561c28da47eb837945c11f7d84664a48f7
euonymus <euonymus0220@gmail.com>
2020-01-22 20:50:10 +0900

    quark delete

--- ui/src/pages/add-quark.js
+++ ui/src/pages/add-quark.js
@@ -1,10 +1,13 @@
 import React from "react";
 import QuarkForm from "../components/form-quark";
+import LoggedinOnly from "../components/loggedin_only";
 
 const AddNewQuark = () => (
-  <div className="AddNewQuark">
-    <h1>Add New Quark</h1>
-    <QuarkForm />
-  </div>
+  <LoggedinOnly>
+    <div className="AddNewQuark">
+      <h1>Add New Quark</h1>
+      <QuarkForm />
+    </div>
+  </LoggedinOnly>
 );
 export default AddNewQuark;
