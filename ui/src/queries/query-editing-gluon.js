42c4ee164d268b86b6a781c5306a86c4b2d839ff
euonymus <euonymus0220@gmail.com>
2020-01-28 01:02:43 +0900

    chore

--- ui/src/queries/query-editing-gluon.js
+++ ui/src/queries/query-editing-gluon.js
@@ -1,12 +1,19 @@
 import _ from "lodash";
 import gql from "graphql-tag";
 import { gluonFields } from "./fields-gluon";
+import { quarkFields } from "./fields-quark";
 
 // NOTE: This is a component to salvage a gluon for editing purpose
 const queryGluonCompiled = _.template(`
   query editingGluon($id: ID) {
     editingGluon(id: $id<%= addingUserIdParam %>) {
       ${gluonFields}
+      active {
+        ${quarkFields}
+      }
+      passive {
+        ${quarkFields}
+      }
     }
   }
 `);
